d8d700847e8054ff3bbdc4a169370c0b57b58888
Kingsley Enoka <kuenoka@mix.wvu.edu>
2020-11-18 11:13:08 -0600

    Initial Commit

---
+++ LinkProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LinkProject
+//
+//  Created by Kingsley Enoka on 11/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
